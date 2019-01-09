0a99c04228008a63ccde07c0e832189ba24f732e
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-09 16:36:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function getRepositories(){
   req.open('GET', 'https://api.github.com/users/alexandrafren/repos');
   req.send();
 }
+
+function showRepositories(){
+  console.log(this.responseText);
+}
