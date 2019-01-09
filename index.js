8e980c558b9f2b78ad1f7fc22193ccf368dc77b1
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-09 16:53:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,11 +29,17 @@ function getCommits(el) {
   req.send();
 }
 
-function showCommits(){
+function showCommits() {
   const commits = JSON.parse(this.responseText);
-  const commitsList = '<ul>${commits.map(
-    commit =>
-    '<li><strong>' + commit.author.login + '</strong> - ' +
-    commit.commit.message + '</li>'
-  ).join('')}</ul>';
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
 }
